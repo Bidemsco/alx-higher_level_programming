84e706bbbc82a52fd8389e26e3135094a72d7bc6
Abidemi Osunsanya <111005519+Bidemsco@users.noreply.github.com>
2022-11-29 14:49:45 +0100

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,6 @@
+utable File  5 lines (5 sloc)  214 Bytes
+
 #!/usr/bin/python3
-
-
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 str = str[39:66] + str[106:107+5] + str[:6]
